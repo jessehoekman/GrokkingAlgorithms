1849db485dc2f1909fdb6b75c00fbfa2dcc32317
jessehoekman <jessehoekman@hotmail.com>
2024-03-15 12:33:18 +0100

    Finish two sum - yay fun!!
    neetcode

--- neetcode/two_sum.py
+++ neetcode/two_sum.py
@@ -5,10 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        i = 0
+        for i in range(len(nums)):
 
-        if (nums[i] + nums[x]) == target
-            return [nums[i].index, nums[x].index]
-        else
+            for x in range(len(nums)):
+                if ((nums[i] + nums[x]) == target):
+                    return [i,x]
+                else:
+                    x += 1
+            else: i += 1
 
 target = 9
-nums = [2,7,11,15] 
+nums = [11,2,15,7]
+
+print(Solution().twoSum(nums,target))
